a45d51aadc6b70801f36d1988a7d3f95da06f705
m-davis <m.davis@tamu.edu>
2021-04-03 04:29:52 -0500

    Added probability based replacement, fixed initial delay issue, and fix some small issues and spacing issues

--- src/bots/MyBot.java
+++ src/bots/MyBot.java
@@ -1,6 +1,6 @@
 package bots;
 
-import java.sql.Time;
+import java.util.Random;
 
 import com.cavariux.twitchirc.Chat.Channel;
 import com.cavariux.twitchirc.Chat.User;
@@ -15,13 +15,16 @@ public class MyBot extends TwitchBot{
 		this.setOauth_Key("oauth:km7xcs8h16m032o7l6ky90z5v9mf48");
 	}
 	
-	final String[] triggers = {"ma", "dese", "dragon", "sucon","sawcon", "saw"};
+	final String[] triggers = {"ma", "dese", "dragon", "sucon","sawcon", "saw", "joe"};
+	Random random = new Random();
 	
-	//FIXME: ADD JOE MAMA
 	//FIXME: ADD "I barely know her"
 	public void onMessage(User user, Channel channelMa, String messageMa) {
 		
-		try { if(Math.abs(System.currentTimeMillis() - Main.getLastMaClock(channelMa)) < 45000) { return; } }
+		try {
+			long cooldownMa = Main.getCooldownMaAmount(channelMa);
+			if( !(cooldownMa == -1) && Math.abs(System.currentTimeMillis() - Main.getLastMaClock(channelMa)) < cooldownMa) { return; } 
+		}
 		catch(Exception e) { System.out.println(e.getMessage()); }
 		
 		try{ Main.setLastMaClock(channelMa); } catch(Exception e) { System.out.println(e.getMessage()); }
@@ -32,12 +35,17 @@ public class MyBot extends TwitchBot{
 			indexMa = messageMa.toLowerCase().lastIndexOf(triggerMa);
 			if(indexMa != -1 
 					&& !(indexMa == 0 && messageMa.length() != triggerMa.length()) 
-					&& (messageMa.charAt(indexMa-1)==' ' || indexMa+triggerMa.length() >= messageMa.length() || messageMa.charAt(indexMa+triggerMa.length())==' ' )
+					&& (indexMa-1 < 0 || messageMa.charAt(indexMa-1)==' ' || indexMa+triggerMa.length() >= messageMa.length() || messageMa.charAt(indexMa+triggerMa.length())==' ' ) 
 			) {
+				try{ if(random.nextInt(Main.getProbabilityMaAmount(channelMa)) != 0) { return; } } catch(Exception e) { System.out.println(e.getMessage()); }
+		
 				newMessageMa = messageMa.substring(0,indexMa) + " " + messageMa.substring(indexMa,indexMa + triggerMa.length());
 				if(triggerMa.equals("saw")) { newMessageMa += " con"; }
-				if(triggerMa.equals("dragon") || triggerMa.equals("sugon") || triggerMa.equals("saw") || triggerMa.equals("sawcon")){ newMessageMa += " dese"; }
-				newMessageMa += " balls lmao "; 
+				if(triggerMa.equals("dragon") || triggerMa.equals("sucon") || triggerMa.equals("saw") || triggerMa.equals("sawcon")){ newMessageMa += " dese"; }
+				if(!triggerMa.equals("joe")) { newMessageMa += " balls"; }
+				else { newMessageMa += " mama"; }
+				if(triggerMa.equals("dragon")) { newMessageMa += " on ya face"; }
+				newMessageMa += " lmao";
 				this.sendMessage(newMessageMa,channelMa);
 				return;
 			}
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,19 +1,21 @@
 package main;
 import bots.MyBot;
+
 import com.cavariux.twitchirc.Chat.Channel;
 
 public class Main {
 	
-	final static String[] channels = {"#exeloar"};
+	final static String[] channels = {"#exeloar", "#capp_kap"};
 	static long[] channelLastMaClocks = new long[channels.length]; 
+	static int[] channelCooldownMaAmounts = new int[channels.length];
+	static int[] probabilityMaReplacements = new int[channels.length];
 	
-	final static long version = 2L;
+	final static long version = 3L;
 	
 	public static long lastMaClock = -1000000L;
 	
-	private static void initializeLastMaClocks() {
-		long valueMa = System.currentTimeMillis() - 45000; 
-		for(int i = 0; i < channels.length; i++) { channelLastMaClocks[i] = valueMa; }
+	private static void initializeLastMaClocks() { 
+		for(int i = 0; i < channels.length; i++) { channelLastMaClocks[i] = -1; }
 	}
 	
 	public static void setLastMaClock(Channel channelMa) throws Exception {
@@ -35,9 +37,52 @@ public class Main {
 		throw new Exception("Channel does not exist");
 	}
 	
+	private static void initializeCooldownMaAmounts() {
+		channelCooldownMaAmounts[0] = 0;
+		for(int i = 1;i<channels.length;i++) {
+			channelCooldownMaAmounts[i] = 5;
+		}
+	}
+
+	public static int getCooldownMaAmount(Channel channelMa) throws Exception {
+		String channelNameMa = channelMa.toString();
+		for(int i = 0; i < channels.length; i++) {
+			if(channelNameMa.equalsIgnoreCase(channels[i])) {
+				return channelCooldownMaAmounts[i];
+			}
+		}
+		throw new Exception("Channel does not exist");
+	}
+	
+	private static void initializeProbabilityMaAmounts() {
+		probabilityMaReplacements[0] = 1;
+		for(int i = 1;i<channels.length;i++) {
+			probabilityMaReplacements[i] = 1;
+		}
+	}
+
+	public static int getProbabilityMaAmount(Channel channelMa) throws Exception {
+		String channelNameMa = channelMa.toString();
+		for(int i = 0; i < channels.length; i++) {
+			if(channelNameMa.equalsIgnoreCase(channels[i])) {
+				return probabilityMaReplacements[i];
+			}
+		}
+		throw new Exception("Channel does not exist");
+	}
+	
+
+	
+	private static void initialize() {
+		initializeLastMaClocks();
+		initializeCooldownMaAmounts();
+		initializeProbabilityMaAmounts();
+	}
+	
 	public static void main(String[]args) {
 		
-		initializeLastMaClocks();
+		initialize();
+
 		MyBot bot = new MyBot();
 		bot.connect();
 		for(String channel : channels) {
